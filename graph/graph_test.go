59853af4bd25d1069448e15ea75af3d002a00400
frank <603128423@qq.com>
2018-10-11 15:17:29 +0100

    init  graph

--- graph/graph_test.go
+++ graph/graph_test.go
@@ -1,1 +1,19 @@
 package test
+
+import (
+	"bufio"
+)
+
+type Node struct {
+	id   int
+	name string
+}
+
+type Graph struct {
+	array []*Node
+}
+
+func NewGraph(scanner *bufio.Scanner, count int) *Graph {
+	array := make([]*Node, count, count)
+	return &Graph{array}
+}
